4e0c31ced22d2e110070a5b6725f76fe9609a5a3
Geraldinne Bohr <geraldinnebohr@gmail.com>
2020-02-03 22:10:24 +0000

    [FIX] pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,10 +5,10 @@ import sys
 
 
 if __name__ == "__main__":
-    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId='
-                         + sys.argv[1])
-    names = requests.get('https://jsonplaceholder.typicode.com/users/'
-                         + sys.argv[1])
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1])
+    names = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         sys.argv[1])
 
     json_todo = to_do.json()
     json_names = names.json()
@@ -16,13 +16,15 @@ if __name__ == "__main__":
     all_tasks = 0
     tasks_completed = 0
 
+    titles_completed = []
     for task in json_todo:
         all_tasks += 1
         if task["completed"] is True:
             tasks_completed += 1
+            titles_completed.append(task["title"])
 
     print('Employee {} is done with tasks({}/{}):'
           .format(json_names['name'], tasks_completed, all_tasks))
 
-    for title_task in json_todo:
-        print('\t {}'.format(title_task["title"]))
+    for title_task in titles_completed:
+        print('\t {}'.format(title_task))
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,10 +6,10 @@ import sys
 
 
 if __name__ == "__main__":
-    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId='
-                         + sys.argv[1])
-    names = requests.get('https://jsonplaceholder.typicode.com/users/'
-                         + sys.argv[1])
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1])
+    names = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         sys.argv[1])
 
     json_todo = to_do.json()
     json_names = names.json()
