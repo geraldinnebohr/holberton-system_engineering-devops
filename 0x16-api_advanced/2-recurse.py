cf2f17bf770dffa1cd34ca1e260f6a4f591e5e58
Geraldinne Bohr <geraldinnebohr@gmail.com>
2020-02-06 23:07:58 +0000

    [FEATURE] recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""get request reddit api"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], page=None):
+    """function that print the titles of the 10 hottest posts"""
+    headers = {"User-Agent": "geraldinnebohr"}
+
+    if page:
+        sub = requests.get('https://reddit.com/r/' + subreddit +
+                               '/hot.json?after=' + page,
+                               headers=headers)
+    else:
+        sub = requests.get('https://reddit.com/r/' + subreddit +
+                               '/hot.json', headers=headers)
+
+    if sub.status_code == 404:
+        return None
+
+    sub_json = sub.json()
+    sub_data = sub_json['data']
+    page = sub_data['after']
+    sub_children = sub_data['children']
+
+    for children in sub_children:
+        children_data = children['data']
+        hot_list.append(children_data['title'])
+
+    if page:
+        recurse(subreddit, hot_list, page)
+        return hot_list
